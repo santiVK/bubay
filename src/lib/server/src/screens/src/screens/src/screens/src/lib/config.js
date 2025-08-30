7b9415845a26512a9bcf3e47e38cc6a117d234ad
santiVK <santi.sesmero@gmail.com>
2025-08-30 12:13:36 +0200

    Create config.js

---
+++ src/lib/server/src/screens/src/screens/src/screens/src/lib/config.js
@@ -0,0 +1,6 @@
+// src/lib/config.js
+
+export const SUPABASE_URL = "https://TU_PROYECTO.supabase.co";
+export const SUPABASE_ANON_KEY = "TU_ANON_KEY";
+
+export const API_BASE_URL = "http://localhost:4242"; // Cambia si usas otro host
